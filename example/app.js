bcc49f4fe0a576c7d925ccc3eb9375390a9522c7
Belelros <sombragriselros@gmail.com>
2013-11-12 22:07:54 +0000

    Initial functionality
    Fixes #1

--- example/app.js
+++ example/app.js
@@ -1,5 +1,36 @@
-angular.module('example-app', []);
+angular.module('example-app', ['notice']);
 
-var MainController = function($scope) {
+var MainController = function($scope, notice) {
+  $scope.message = 'Shown message';
+  $scope.type = 'info';
+  $scope.isSticky = false;
 
+  $scope.availableLevels = [
+    {
+      'name' : 'Information',
+      'value' : 'info'
+    },
+    {
+      'name' : 'Success',
+      'value' : 'success'
+    },
+    {
+      'name' : 'Warning',
+      'value' : 'warning'
+    },
+    {
+      'name' : 'Error',
+      'value' : 'error'
+    }
+  ];
+
+  $scope.show = function(){
+    var method = $scope.isSticky ? 'showSticky' : 'show';
+
+    notice[method]($scope.message,$scope.type);
+  };
+
+  $scope.hide = function(){
+    notice.hide();
+  };
 };

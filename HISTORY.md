9080115d6f46f6fad66d7bece48d1be395b968c8
Greg Allen <me@jga.me>
2013-11-22 08:26:51 -0800

    Release 0.1.0

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+
+0.1.0 / 2013-11-22 
+==================
+
+  * Updating module name to ftNotice
+  * Initial functionality
+  * project setup
--- dist/angular-notice.js
+++ dist/angular-notice.js
@@ -6,4 +6,28 @@
  * copyright First + Third 2013
  * MIT License
 */
-//angular-notice.js
+(function(){
+  angular.module('ftNotice',[])
+      .service('ftNotice', [function(){
+        this.defaultLevel = 'info';
+
+        this.show = function(message, level, options){
+          options = options || {};
+          level = level || this.defaultLevel;
+
+          $.notice(message,angular.extend({
+            level : level
+          },options));
+        };
+
+        this.showSticky = function(message,level){
+          this.show(message,level, {
+            timeout : false
+          });
+        };
+
+        this.hide = function(){
+          this.show('close');
+        };
+      }]);
+})();
